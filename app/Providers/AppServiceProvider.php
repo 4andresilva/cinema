d4f732de1690ffc295802666f84bb3f02987bcfc
suporteandresilva <suporteandresilva@gmail.com>
2025-11-12 03:23:56 +0000

    retornando com o https forçado

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->environment('production')) {
-            // URL::forceScheme('https');
+            $forwardedProto = $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? null;
+            $https = $_SERVER['HTTPS'] ?? null;
+
+            if ($forwardedProto === 'https' || $https === 'on') {
+                URL::forceScheme('https');
+            }
         }
     }
 }
